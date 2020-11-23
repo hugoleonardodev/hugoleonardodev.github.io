8a3a32db575a631b92af77568c8fdec75cd41bc3
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-22 23:35:01 -0300

    Button home and Button Projects ok

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
+const mainContent = document.querySelector('.main-content');
 const hero = document.querySelector('.hero');
 const projectsIndex = document.querySelector('.projects-index');
+const buttonLaunch = document.querySelector('.button-launch');
 const buttonProjects = document.querySelector('.button-projects');
+const showingContent = document.createElement('section');
+showingContent.className = 'content-container';
+
+buttonLaunch.addEventListener('click', function () {
+	alert('Hello World! My name is Hugo. If you want to know more about me, feel free to check one of projects or social medias. You can also click on Info button. Thanks for come.');
+	projectsIndex.style.display = 'none'
+	hero.style.display = 'flex'
+		// click.preventDefault();
+})
 
 buttonProjects.addEventListener('click', function () {
 	hero.style.display = 'none';
