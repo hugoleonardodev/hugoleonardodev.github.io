cca6da95483e0a43dd451f5281e8cc6e9a321d3f
Hugo Leonardo <hugoleonardo.dev@gmail.com>
2021-12-19 18:58:06 -0300

    Updates

--- src/stories/utils/getShape.ts
+++
@@ -1,7 +0,0 @@
-const getShape = (shape: 'flat' | 'borders' | 'rounded') => {
-  if (shape === 'flat') return '0';
-  if (shape === 'borders') return '8px';
-  return '100px';
-}
-
-export default getShape;
